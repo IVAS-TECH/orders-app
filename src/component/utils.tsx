1e142f94e4b0ba31a72d7a23d0405b78a42274fe
NoHomey <ivo.stratev.tues@gmail.com>
2019-09-05 11:05:32 +0300

    refactoring (code formating)

--- src/component/utils.tsx
+++ src/component/utils.tsx
@@ -5,7 +5,8 @@ type GetProps<T> = T extends React.FC<infer P> ? P : {};
 export function configure<
     ComponentType extends React.FC<GetProps<ComponentType>>,
     ConfigureProps extends Partial<GetProps<ComponentType>>
->(Component: ComponentType, configureProps: ConfigureProps): React.FC<Omit<GetProps<ComponentType>, keyof ConfigureProps>> {
+>(Component: ComponentType, configureProps: ConfigureProps)
+: React.FC<Omit<GetProps<ComponentType>, keyof ConfigureProps>> {
     return props => {
         const allProps = {...configureProps, ...props} as GetProps<ComponentType>;
         return <Component {...allProps} />;
